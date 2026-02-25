a23c87905e0b9db564e5a4512d1769aa47ad2cdd
Alvbatross <alvaropato54@gmail.com>
2026-02-25 14:35:52 +0800

    Patient Interview Scene UI is loaded properly. TODO: Finish fully functional UI Scene

--- src/main.gd
+++ src/main.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var loading_screen := $LoadingScreenLayer/LoadingScreen
 @onready var main_menu := $MainMenuLayer/MainMenu
 
+
 func _ready() -> void:
 	loading_screen.start_loading_screen()
 	await get_tree().create_timer(5).timeout
