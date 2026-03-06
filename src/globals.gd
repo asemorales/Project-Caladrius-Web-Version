61e773c7c8ace41abd02b49e436b91a35c879fce
JustALonePotato <55743527+JustALonePotato@users.noreply.github.com>
2026-03-06 23:11:48 +0800

    Saved settings to globals for easier access. Encrypted cookies..

--- src/globals.gd
+++ src/globals.gd
@@ -2,7 +2,13 @@ extends Node
 
 signal secrets_loaded
 
-
+# Secret
 var api_keys: Dictionary = { }
 var key_file: Dictionary = { }
 var key_file2: Dictionary = { }
+var master_password: String = "HsZ#Aefxj&H&8u$gd^$UU$%5y5j!UK6BjCXWMmpsne#^GNDda!BcMWhVYZd@$*sKD9@v57GGNKzc4WhW!2tp%tzzCogoYkdT6@c4RVsK8qm4porTEhh63UC6vLMvk9s7"
+
+# Settings
+var tts: int
+var stt: int
+var volume: float
--- src/ui-scenes/settings_menu.gd
+++ src/ui-scenes/settings_menu.gd
@@ -3,9 +3,6 @@ extends MarginContainer
 var _audio_stream_player: AudioStreamPlayer
 var _is_mic_on: bool = false
 var _settings_ver: String = "1.0.0"
-var volume: float
-var tts: int
-var stt: int
 
 @onready var volume_slider: HSlider = $VBoxContainer/CenterContainer/MarginContainer/HBoxContainer/VBoxContainer2/VolumeSlider
 @onready var text_to_speech_option: OptionButton = $VBoxContainer/CenterContainer/MarginContainer/HBoxContainer/VBoxContainer2/TextToSpeechOption
@@ -13,6 +10,7 @@ var stt: int
 @onready var microphone_option: OptionButton = $VBoxContainer/CenterContainer/MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/MicrophoneOption
 @onready var microphone_button: Button = $VBoxContainer/CenterContainer/MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/Button
 @onready var main_menu: Control = get_node(".").get_parent()
+
 
 func _ready() -> void:
 	# Mute the bus used to test the mic
@@ -44,25 +42,25 @@ func _ready() -> void:
 			"mic": "default",
 			"version": _settings_ver
 		}
-		file_access = FileAccess.open("user://settings_test", FileAccess.WRITE)
+		file_access = FileAccess.open_encrypted_with_pass("user://settings", FileAccess.WRITE, Globals.master_password)
 		file_access.store_line(JSON.stringify(settings_data))
 		file_access.close()
 	
 	var data = _load_settings()
 	
 	# Set the global vars
-	volume = data["volume"]
-	tts = data["tts"]
-	stt = data["stt"]
+	Globals.volume = data["volume"]
+	Globals.tts = data["tts"]
+	Globals.stt = data["stt"]
 
 	# Update UI to match loaded settings values
-	volume_slider.value = volume
-	text_to_speech_option.select(tts)
-	speech_to_text_option.select(stt)
+	volume_slider.value = Globals.volume
+	text_to_speech_option.select(Globals.tts)
+	speech_to_text_option.select(Globals.stt)
 
 
 func _is_settings_valid() -> bool:
-	if not FileAccess.file_exists("user://settings_test"):
+	if not FileAccess.file_exists("user://settings"):
 		return false
 	
 	var data = _load_settings()
@@ -75,27 +73,27 @@ func _is_settings_valid() -> bool:
 func _load_settings() -> Variant:
 	# Get the settings data
 	var json: JSON = JSON.new()
-	var file_access = FileAccess.open("user://settings_test", FileAccess.READ)
+	var file_access = FileAccess.open_encrypted_with_pass("user://settings", FileAccess.READ, Globals.master_password)
 	var json_string: String = file_access.get_line()
 	file_access.close()
 
 	# Parse the settings data
 	var json_parse_result: int = json.parse(json_string)
 	if not json_parse_result == OK:
-		printerr("settings_test can't be parsed as a json object")
+		printerr("settings can't be parsed as a json object")
 		return { }
 	return json.data
 
 
 func _save_settings() -> void:
 	var settings_data = {
-		"volume": volume,
-		"tts": tts,
-		"stt": stt,
+		"volume": Globals.volume,
+		"tts": Globals.tts,
+		"stt": Globals.stt,
 		"mic": "default",
 		"version": _settings_ver
 	}
-	var file_access = FileAccess.open("user://settings_test", FileAccess.WRITE)
+	var file_access = FileAccess.open_encrypted_with_pass("user://settings", FileAccess.WRITE, Globals.master_password)
 	file_access.store_line(JSON.stringify(settings_data))
 	file_access.close()
 
@@ -126,15 +124,15 @@ func _on_volume_slider_drag_ended(value_changed: bool) -> void:
 	if not value_changed:
 		return
 	
-	volume = volume_slider.value
+	Globals.volume = volume_slider.value
 
 
 func _on_text_to_speech_option_selected(index: int) -> void:
-	tts = index
+	Globals.tts = index
 
 
 func _on_speech_to_text_option_selected(index: int) -> void:
-	stt = index
+	Globals.stt = index
 
 
 func _on_save_and_exit_button_pressed() -> void:
