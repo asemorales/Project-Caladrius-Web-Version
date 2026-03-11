aab2bc89f404592b91a92b0775ad7390a0cb04b4
Alvbatross <alvaropato54@gmail.com>
2026-03-11 12:00:01 +0800

    Added thinking animation

--- src/patient-interview-scenes/patient.gd
+++ src/patient-interview-scenes/patient.gd
@@ -1,3 +1,13 @@
 extends Node2D
 
 class_name Patient
+
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+
+func play_idle() -> void:
+	anim.play("idle")
+
+
+func play_thinking() -> void:
+	anim.play("thinking")
