30817c855a6d6e59de7190372be90aec21b6b01e
Alvbatross <alvaropato54@gmail.com>
2026-04-02 10:49:05 +0800

    Female Patient fully Rigged and Animated

--- src/patient-interview-scenes/patient_model.gd
+++ src/patient-interview-scenes/patient_model.gd
@@ -3,7 +3,10 @@ extends Node2D
 class_name PatientModel
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var face: AnimatedSprite2D = $Polygons/Face
 
+var previously_blinked : bool = false
+var thinking_left : bool = true
 
 func play_idle() -> void:
 	anim.play("idle")
@@ -11,3 +14,41 @@ func play_idle() -> void:
 
 func play_thinking() -> void:
 	anim.play("thinking")
+
+
+#---------------------------------------
+#          FACIAL ANIMATIONS
+#---------------------------------------
+func face_play_blink() -> void:
+	if not previously_blinked:
+		face.play("blink")
+	
+	previously_blinked = not previously_blinked
+
+func face_play_thinking() -> void:
+	if thinking_left:
+		face.play("thinking_left")
+	else:
+		face.play("thinking_right")
+
+func face_play_think_transition() -> void:
+	if thinking_left:
+		face.play("think_transition_to_right")
+	else:
+		face.play("think_transition_to_left")
+	
+	thinking_left = not thinking_left
+
+
+func face_play_talking() -> void:
+	face.play("talking")
+
+func face_play_default() -> void:
+	face.play("default")
+
+func _on_face_animation_finished() -> void:
+	if face.animation == "blink":
+		face.play("default")
+	
+	if face.animation == "think_transition_to_right" or face.animation == "think_transition_to_left":
+		face_play_thinking()
